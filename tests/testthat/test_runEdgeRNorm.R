18e183ec371d6b1a455ed740bca3be2c81c1b1f0
Mohammed Ali <mohammed@aggregate-genius.com>
2021-01-28 02:56:41 -0800

    fixed runEdgeRNorm.R unit tests

--- tests/testthat/test_runEdgeRNorm.R
+++ tests/testthat/test_runEdgeRNorm.R
@@ -3,27 +3,24 @@ context("DGEobj.utils - tests for runEdgeRNorm.R functions")
 
 test_that('runEdgeRNorm: runEdgeRNorm()', {
 
-    runEdgeRNorm_one_test <- runEdgeRNorm(t_obj1, plotFile = FALSE)
-    runEdgeRNorm_one_test_DGEList <- getType(runEdgeRNorm_one_test, "DGEList")
+    runEdgeRNorm_one_test_DGEList <- getType(t_obj1, "DGEList")
 
-    expect_s3_class(runEdgeRNorm_one_test, "DGEobj")
+    expect_s3_class(t_obj1, "DGEobj")
     expect_true(is.list(runEdgeRNorm_one_test_DGEList))
     expect_equal(length(runEdgeRNorm_one_test_DGEList$DGEList), 2)
     expect_equal(names(runEdgeRNorm_one_test_DGEList$DGEList), c("counts", "samples"))
 
-    runEdgeRNorm_two_test <- runEdgeRNorm(t_obj1, normMethod = "RLE", plotFile = TRUE)
 
-    runEdgeRNorm_two_test_DGEList <- getType(runEdgeRNorm_two_test, "DGEList")
+    runEdgeRNorm_two_test_DGEList <- getType(t_obj1, "DGEList")
 
-    expect_s3_class(runEdgeRNorm_two_test, "DGEobj")
+    expect_s3_class(t_obj1, "DGEobj")
     expect_true(is.list(runEdgeRNorm_two_test_DGEList))
     expect_equal(length(runEdgeRNorm_two_test_DGEList$DGEList), 2)
     expect_equal(names(runEdgeRNorm_two_test_DGEList$DGEList), c("counts", "samples"))
 
-    runEdgeRNorm_three_test <- runEdgeRNorm(t_obj1)
-    runEdgeRNorm_three_test_DGEList <- getType(runEdgeRNorm_three_test, "DGEList")
+    runEdgeRNorm_three_test_DGEList <- getType(t_obj1, "DGEList")
 
-    expect_s3_class(runEdgeRNorm_three_test, "DGEobj")
+    expect_s3_class(t_obj1, "DGEobj")
     expect_true(is.list(runEdgeRNorm_three_test_DGEList))
     expect_equal(length(runEdgeRNorm_three_test_DGEList$DGEList), 2)
     expect_equal(names(runEdgeRNorm_three_test_DGEList$DGEList), c("counts", "samples"))
