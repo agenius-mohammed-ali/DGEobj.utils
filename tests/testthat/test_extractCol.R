48828fc696ae6e8bf744b67925a908217ae855b4
Mohammed Ali <mohammed@aggregate-genius.com>
2021-01-28 20:15:34 -0800

    Fixed extractCol.R unit tests

--- tests/testthat/test_extractCol.R
+++ tests/testthat/test_extractCol.R
@@ -7,16 +7,16 @@ test_that('extractCol: extractCol()', {
     extractCol_one_test <- extractCol(extractCol_contrastList, colName = "P.Value")
 
     expect_true(is.data.frame(extractCol_one_test))
-    expect_equal(nrow(extractCol_one_test), 11709)
+    expect_equal(nrow(extractCol_one_test), 946)
     expect_equal(ncol(extractCol_one_test), 2)
-    expect_equal(names(extractCol_one_test), c("BMTL", "BMTH"))
+    expect_equal(names(extractCol_one_test), c("BDL_vs_Sham", "EXT1024_vs_BDL"))
 
     extractCol_two_test <- extractCol(extractCol_contrastList, colName = "P.Value", robust = FALSE)
 
     expect_true(is.data.frame(extractCol_two_test))
-    expect_equal(nrow(extractCol_two_test), 11709)
+    expect_equal(nrow(extractCol_two_test), 946)
     expect_equal(ncol(extractCol_two_test), 2)
-    expect_equal(names(extractCol_two_test), c("BMTL", "BMTH"))
+    expect_equal(names(extractCol_two_test), c("BDL_vs_Sham", "EXT1024_vs_BDL"))
 
     expect_error(extractCol(extractCol_three_test),
                  regexp = "object 'extractCol_three_test' not found")
