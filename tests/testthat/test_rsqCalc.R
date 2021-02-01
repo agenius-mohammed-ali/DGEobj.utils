a35943f8599a966b56b1bd1eeea359acdcc6d815
Mohammed Ali <mohammed@aggregate-genius.com>
2021-01-31 22:22:53 -0800

    replacing old MArrayLM object with the new one

--- tests/testthat/test_rsqCalc.R
+++ tests/testthat/test_rsqCalc.R
@@ -6,7 +6,7 @@ test_that("rsqCalc.R: rsqCalc()", {
 
     dgelist <- getItem(t_obj1, "DGEList")
     log2cpm <- cpm(dgelist, log = TRUE)
-    rsq <- rsqCalc(log2cpm, t_obj1$RG_fit)
+    rsq <- rsqCalc(log2cpm, t_obj1$ReplicateGroupDesign_fit)
     expect_type(rsq, "double")
 
     expect_error(rsqCalc(1:10), regexp = "normMatrix must be of class 'data.frame' or 'matrix'.")
