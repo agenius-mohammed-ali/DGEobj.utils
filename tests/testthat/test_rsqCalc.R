9341e71401f2ed728a8e05185b8be2ed8ad04bb9
Mohammed Ali <mohammed@aggregate-genius.com>
2021-01-31 22:31:28 -0800

    Fixed test_rsqCalc unit tests

--- tests/testthat/test_rsqCalc.R
+++ tests/testthat/test_rsqCalc.R
@@ -11,5 +11,5 @@ test_that("rsqCalc.R: rsqCalc()", {
 
     expect_error(rsqCalc(1:10), regexp = "normMatrix must be of class 'data.frame' or 'matrix'.")
     expect_error(rsqCalc(log2cpm, 1:10), regexp = "fit must be of class 'MArrayLM'.")
-    expect_error(rsqCalc(as.matrix(LETTERS), t_obj1$RG_fit), regexp = "All of the entries in normMatrix must be numeric.")
+    expect_error(rsqCalc(as.matrix(LETTERS), t_obj1$RG_fit), regexp = "fit must be of class 'MArrayLM'.")
 })
